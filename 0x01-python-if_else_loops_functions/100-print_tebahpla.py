6e3c5507cc0efb55edb7e1a78bb32469a7c932f4
samad <abdessamad.bouchelga@gmail.com>
2023-06-06 14:38:21 -0700

    alphabet3

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{}{}".format(chr(i), chr(i - 32)), end="")
+for i in range(122, 96, -2):
+    print("{}{}".format(chr(i), chr(i - 33)), end="")
